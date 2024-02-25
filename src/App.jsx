ee0b694d4f31d2c265806376db3f321f224ae3b0
Yelish <118233081+GrapeJuice11111@users.noreply.github.com>
2024-02-25 01:14:06 -0600

    Create A Form Component For CV Profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import { useState } from "react";
 import "./App.css";
+import Form from "./components/Form.jsx";
 
-function App() {}
+function App() {
+  return (
+    <>
+      <Form />
+    </>
+  );
+}
 
 export default App;
